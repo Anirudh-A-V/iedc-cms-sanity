86a6fe6361f4521eb5639d28925bbc5e49af77f9
Anirudh A V <anirudh.av02@gmail.com>
2022-11-15 13:18:46 +0530

    Initial commit

---
+++ schemas/achievements.js
@@ -0,0 +1,25 @@
+export default {
+    name: 'achievements',
+    title: 'Achievements',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name:'imgUrl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text'
+        },
+    ]
+}
